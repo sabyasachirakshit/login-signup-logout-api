0d73a574cd3c64c4f80013263751626ca7d1104a
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2023-09-10 21:37:11 +0530

    mongoose code written, currently trying to connect

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyparser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const db = require("./config/config").get(process.env.NODE_ENV);
 
 const app = express();
 
 app.use(bodyparser.urlencoded({ extended: "false" }));
 app.use(bodyparser.json());
 app.use(cookieParser());
+
+//database connection
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  db.DATABASE,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function (err) {
+    if (err) console.log(err);
+  }
+);
 
 app.get("/", function (req, res) {
   res.status(200).send("Welcome to login, sign up api");
