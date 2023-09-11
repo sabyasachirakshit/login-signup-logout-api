1d076d2f71757986ee99595c2718e969050ed422
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2023-09-11 11:27:51 +0530

    comparing passwords and token function written

--- models/user.js
+++ models/user.js
@@ -56,4 +56,46 @@ userSchema.pre("save", function (next) {
   }
 });
 
+//comparing passwords
+userSchema.methods.comparepassword = function (password, cb) {
+  bcrypt.compare(password, this.password, function (err, isMatch) {
+    if (err) return cb(next);
+    cb(null, isMatch);
+  });
+};
+
+//generating token
+userSchema.methods.generateToken = function (cb) {
+  var user = this;
+  var token = jwt.sign(user._id.toHexString(), confiq.SECRET);
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(next);
+    cb(null, user);
+  });
+};
+
+//finding user by token
+userSchema.statics.findByToken = function (token, cb) {
+  var user = this;
+
+  jwt.verify(token, confiq.SECRET, function (err, decode) {
+    user.findOne({ _id: decode, token: token }, function (err, user) {
+      if (err) return cb(err);
+      cb(null, user);
+    });
+  });
+};
+
+//delete token
+userSchema.methods.deleteToken = function (token, cb) {
+  var user = this;
+
+  user.update({ $unset: { token: 1 } }, function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
+
 module.exports = mongoose.model("User", userSchema);
