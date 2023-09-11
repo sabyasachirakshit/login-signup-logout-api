e9b2c0358da007f1cceced5af414c8b1d7998f9d
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2023-09-11 11:36:43 +0530

    login logout get routes defined

--- index.js
+++ index.js
@@ -95,6 +95,24 @@ app.post("/api/login", function (req, res) {
   });
 });
 
+// get logged in user
+app.get("/api/profile", auth, function (req, res) {
+  res.json({
+    isAuth: true,
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.firstname + req.user.lastname,
+  });
+});
+
+//logout user
+app.get("/api/logout", auth, function (req, res) {
+  req.user.deleteToken(req.token, (err, user) => {
+    if (err) return res.status(400).send(err);
+    res.sendStatus(200);
+  });
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
