8ec79aff45e8b020bcb09e2cfe161fd2ff8adfba
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2023-09-11 11:21:07 +0530

    hash function written for password protection

--- models/user.js
+++ models/user.js
@@ -1,4 +1,8 @@
 var mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const confiq = require("../config/config").get(process.env.NODE_ENV);
+const salt = 10;
 
 const userSchema = mongoose.Schema({
   firstname: {
@@ -30,6 +34,26 @@ const userSchema = mongoose.Schema({
   token: {
     type: String,
   },
+});
+
+userSchema.pre("save", function (next) {
+  var user = this;
+
+  if (user.isModified("password")) {
+    bcrypt.genSalt(salt, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+
+        user.password = hash;
+        user.password2 = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
 });
 
 module.exports = mongoose.model("User", userSchema);
